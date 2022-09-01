3c2d86c736e67b36ef7f9d650135ba903d44136e
OhmPatil <workpatil10@gmail.com>
2022-09-01 22:53:32 +0530

    Logic to push and get data from firestore

---
+++ src/AddTask.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { database } from "./firebase";
+import { collection, addDoc, query, Timestamp } from "firebase/firestore";
+
+const AddTask = (props) => {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [isCompleted, setIsCompleted] = useState(false);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    try {
+      await addDoc(collection(database, "tasks"), {
+        title: title,
+        description: description,
+        isCompleted: isCompleted,
+        created: Timestamp.now(),
+      });
+    } catch (error) {
+      alert(error);
+    }
+
+    setTitle("");
+    setDescription("");
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="title"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+          placeholder="Enter title"
+        />
+
+        <input
+          type="text"
+          name="description"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+          placeholder="Enter Description"
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddTask;
--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import './App.css';
+import { useEffect, useState } from "react";
+import { database } from "./firebase";
+import { collection, addDoc, Timestamp, query, orderBy, onSnapshot } from "firebase/firestore";
+import "./App.css";
+import AddTask from "./AddTask";
 
 function App() {
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    const q = query(collection(database, 'tasks'), orderBy('created', "desc"))
+    onSnapshot(q, (querySnapshot) => {
+      setTasks(querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        data: doc.data()
+      })))
+    })
+  }, [])
+
+  console.log(tasks);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AddTask/>
+      {tasks.map(task => {
+        return <pre key={task.id}>{task.data.title}</pre>
+      })}
     </div>
   );
 }
