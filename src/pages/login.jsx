32da84f8f0db6b81a941805e11d02514993d72f1
OhmPatil <workpatil10@gmail.com>
2022-09-02 17:24:45 +0530

    Install tailwindCSS and configure stuff

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login page</div>
+    <div className='bg-gradient-to-r from-sky-500 to-indigo-500 w-screen h-screen'>
+        <div className='text-4xl text-center'>Login Page</div>
+    </div>
   )
 }
 
