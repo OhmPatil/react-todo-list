e2c1504a481fdd1e37824087c9b3580e164ac4fb
OhmPatil <workpatil10@gmail.com>
2022-09-09 17:09:22 +0530

    Cleanup code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-import { useEffect, useState } from "react";
-import { database, auth } from "./firebase";
-import { collection, addDoc, Timestamp, query, orderBy, onSnapshot, where } from "firebase/firestore";
 import "./App.css";
-import AddTask from "./components/AddTask";
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import Login from "./pages/login";
 import Dashboard from './pages/dashboard'
@@ -10,15 +6,6 @@ import Register from "./pages/register";
 import ResetPass from "./pages/reset-password";
 
 function App() {
-  // useEffect(() => {
-  //   const q = query(collection(database, 'tasks'),orderBy('created', "desc"))
-  //   onSnapshot(q, (querySnapshot) => {
-  //     setTasks(querySnapshot.docs.map(doc => ({
-  //       id: doc.id,
-  //       data: doc.data()
-  //     })))
-  //   })
-  // }, [])
 
   return (
     <BrowserRouter>
